fc325652bf07288665f43ea56b9115e9abe58851
FilMich <109309419+FilMich@users.noreply.github.com>
2025-01-04 18:50:51 +0100

    Enemy
    added enemy and wavepoits + scripts for them

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int indexOfWavepoint = 0;
+    
+    void Start()
+    {
+        target = WayPoints.waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World); //normalization kvoli relativnej rychlosti a vzdialenosti k cielu  
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f) {
+
+            if (indexOfWavepoint >= WayPoints.waypoints.Length -1) { 
+                Destroy(gameObject);
+                return;
+            }
+
+            indexOfWavepoint++;
+            target = WayPoints.waypoints[indexOfWavepoint];
+
+		}
+    }
+}
